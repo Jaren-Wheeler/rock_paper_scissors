cd8b482c9888f7aaa969689718d7c1f71f308bbb
Jaren-Wheeler <git@github.com>
2023-11-25 14:13:24 -0700

    Write function that randomly selects a choice for the computer between rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hiiiii");
+
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let randomChoice = Math.floor(Math.random() * choices.length)
+    return choices[randomChoice]
+}
+
+getComputerChoice();
