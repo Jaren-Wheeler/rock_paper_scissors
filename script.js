1eda4b8d5e8b5f892f1474915565b06157c02301
Jaren-Wheeler <git@github.com>
2023-11-25 14:36:27 -0700

    Add rock, paper, scissors round logic in a function. Add a main execution function which runs the program.

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+/* Written by Jaren Wheeler */
 
+// The computers choice
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
     let randomChoice = Math.floor(Math.random() * choices.length)
     return choices[randomChoice]
 }
 
-getComputerChoice();
+// Goes through a round of the game and builds the logic of which choices win
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Paper" ) {
+        return computerSelection + " beats " + playerSelection + ", you lose!"
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return computerSelection + " beats " + playerSelection + ", you lose!"
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return computerSelection + " beats " + playerSelection + ", you lose!"
+    } else if (playerSelection == computerSelection) {
+        return "You have both chosen " + playerSelection + ". Keep playing!"
+    } else {
+        return playerSelection + " beats " + computerSelection + ", you win!"
+    }
+}
+
+
+//Main execution of the game
+function main() {
+    computerChoice = getComputerChoice();
+    playerChoice = prompt("Rock, paper or scissors? ")
+    round = playRound(playerChoice, computerChoice)
+    return round
+}
+
+main()
+
